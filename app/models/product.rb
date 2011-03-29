2515644f61f99f03fd1031053bc6cc46f0a4ddf7
Marc Egli <frog32@me.com>
2011-03-29 16:02:09 +0200

    a day of work

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,5 @@
 class Product < ActiveRecord::Base
+  default_scope :order => 'title'
   validates :title, :description, :image_url, :presence => true
   validates :price, :numericality => {:greater_than_or_equal_to => 0.01, :less_than => 10000}
   validates :image_url, :format => {
